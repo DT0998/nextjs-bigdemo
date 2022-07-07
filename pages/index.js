ab25f3869b95fdaee8cdd1e0fe791cce89ea3340
DT0998 <duongkytrieuvn@gmail.com>
2022-07-07 11:34:58 +0700

    update static site generation

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import Layout from "../components/layout/Layout";
+import MeetupList from "../components/meetups/MeetupList";
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5,12345 Some City",
+    description: "This is first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 10,12345 Some City",
+    description: "This is first meetup!",
+  },
+];
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  // fetch data from a api
+  return {
+    props: {
+      meetups:DUMMY_MEETUPS
+    },
+    revalidate:10
+  };
+
+}
+
+export default HomePage;
